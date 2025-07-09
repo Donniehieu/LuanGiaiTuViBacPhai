545162cdc526f37674c60dd46a9faacaecad04aa
DevHayer <nguyenhieu.mktunity@gmail.com>
2025-07-09 22:16:26 +0700

    Luân bỏ phụ tinh đơn thủ tại cung Nô Bộc

--- luannoboc.js
+++ luannoboc.js
@@ -2116,12 +2116,7 @@ function LuanCachCucXuongKhucNoBoc(keyArr) {
 
     const tyhoi = ["Tỵ", "Hợi"];
     const danmao = ["Dần", "Mão"];
-    for (let i = 0; i < XuongKhuc.length; i++) {
-        if (isSaoToaThuTaiCung("Nô Bộc", XuongKhuc[i])) {
-            console.log(`${XuongKhuc[i]} toạ thủ cung Nô Bộc`);
-            keyArr.push(`${XuongKhuc[i]} toạ thủ cung Nô Bộc`);
-        }
-    }
+   
     for (let i = 0; i < dac_dia.length; i++) {
         for (let j = 0; j < XuongKhuc.length; j++) {
             if (isSaoToaThuTaiCungVaChi("Nô Bộc", dac_dia[i], XuongKhuc[j])) {
@@ -2203,8 +2198,7 @@ function LuanCachCucKhoiVietNoBoc(keyArr) {
 
     for (let i = 0; i < KhoiViet.length; i++) {
         if (isSaoToaThuTaiCung("Nô Bộc", KhoiViet[i])) {
-            console.log(`${KhoiViet[i]} toạ thủ cung Nô Bộc`);
-            keyArr.push(`${KhoiViet[i]} toạ thủ cung Nô Bộc`);
+           
             if (kiemTraCachCuc(KhoiViet[i], tuphuvutuongxuongkhuctahuukhoaquyenloc)) {
                 console.log(`${KhoiViet[i]} toạ thủ cung Nô Bộc gặp`, tuphuvutuongxuongkhuctahuukhoaquyenloc.join(", "));
                 keyArr.push(`${KhoiViet[i]} toạ thủ cung Nô Bộc gặp`, tuphuvutuongxuongkhuctahuukhoaquyenloc.join(", "));
@@ -2262,8 +2256,7 @@ function LuanCachCucLocTonNoBoc(keyArr) {
     const khongkiephaokypha = ["Địa Không", "Địa Kiếp", "Đại Hao", "Tiểu Hao", "Hóa Kỵ", "Tuế Phá"];
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Lộc Tồn")) {
-        console.log("Lộc Tồn toạ thủ cung Nô Bộc");
-        keyArr.push("Lộc Tồn toạ thủ cung Nô Bộc");
+       
         if (kiemTraCachCuc("Lộc Tồn", tuphuxuongkhuckhoivietma)) {
             console.log("Lộc Tồn toạ thủ cung Nô Bộc gặp", tuphuxuongkhuckhoivietma.join(", "));
             keyArr.push("Lộc Tồn toạ thủ cung Nô Bộc gặp", tuphuxuongkhuckhoivietma.join(", "));
@@ -2330,8 +2323,7 @@ function LuanCachCucTaHuuNoBoc(keyArr) {
 
     for (let i = 0; i < TaHuu.length; i++) {
         if (isSaoToaThuTaiCung("Nô Bộc", TaHuu[i])) {
-            console.log(`${TaHuu[i]} toạ thủ cung Nô Bộc`);
-            keyArr.push(`${TaHuu[i]} toạ thủ cung Nô Bộc`);
+           
             if (kiemTraCachCuc(TaHuu[i], tuphuxuongkhuckhoivietkhoaquyenloc)) {
                 console.log(`${TaHuu[i]} toạ thủ cung Nô Bộc gặp`, tuphuxuongkhuckhoivietkhoaquyenloc.join(", "));
                 keyArr.push(`${TaHuu[i]} toạ thủ cung Nô Bộc gặp`, tuphuxuongkhuckhoivietkhoaquyenloc.join(", "));
@@ -2408,8 +2400,7 @@ function LuanCachCucKinhDuongDaLaNoBoc(keyArr) {
     const tumo = ["Thìn", "Tuất", "Sửu", "Mùi"];
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Kình Dương")) {
-        console.log("Kình Dương toạ thủ cung Nô Bộc");
-        keyArr.push("Kình Dương toạ thủ cung Nô Bộc");
+       
         if (kiemTraCachCuc("Kình Dương", ["Hóa Kỵ", "Liêm Trinh", "Cự Môn"])) {
             console.log("Kình Dương toạ thủ cung Nô Bộc gặp Hóa Kỵ, Liêm Trinh, Cự Môn");
             keyArr.push("Kình Dương toạ thủ cung Nô Bộc gặp Hóa Kỵ, Liêm Trinh, Cự Môn");
@@ -2485,8 +2476,7 @@ function LuanCachCucKinhDuongDaLaNoBoc(keyArr) {
     }
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Đà La")) {
-        console.log("Đà La toạ thủ cung Nô Bộc");
-        keyArr.push("Đà La toạ thủ cung Nô Bộc");
+        
         if (kiemTraCachCuc("Đà La", ["Hỏa Tinh", "Linh Tinh", "Kình Dương", "Địa Không", "Địa Kiếp"])) {
             console.log("Đà La toạ thủ cung Nô Bộc gặp Hỏa Tinh, Linh Tinh, Kình Dương, Địa Không, Địa Kiếp");
             keyArr.push("Đà La toạ thủ cung Nô Bộc gặp Hỏa Tinh, Linh Tinh, Kình Dương, Địa Không, Địa Kiếp");
@@ -2523,8 +2513,7 @@ function LuanCachCucHoaLinhNoBoc(keyArr) {
 
     for (let i = 0; i < HoaLinh.length; i++) {
         if (isSaoToaThuTaiCung("Nô Bộc", HoaLinh[i])) {
-            console.log(`${HoaLinh[i]} toạ thủ cung Nô Bộc`);
-            keyArr.push(`${HoaLinh[i]} toạ thủ cung Nô Bộc`);
+           
             if (kiemTraCachCuc(HoaLinh[i], ["Hóa Kỵ", "Liêm Trinh", "Cự Môn"])) {
                 console.log(`${HoaLinh[i]} toạ thủ cung Nô Bộc gặp Hóa Kỵ, Liêm Trinh, Cự Môn`);
                 keyArr.push(`${HoaLinh[i]} toạ thủ cung Nô Bộc gặp Hóa Kỵ, Liêm Trinh, Cự Môn`);
@@ -2608,13 +2597,7 @@ function LuanCachCucKhongKiepNoBoc(keyArr) {
     const tusinh = ["Dần", "Thân", "Tỵ", "Hợi"];
     const cunhatphukhockhach = ["Cự Môn", "Thái Dương", "Thiên Phủ", "Thiên Khốc", "Thiên Hư"];
 
-    for (let i = 0; i < khongkiep.length; i++) {
-        if (isSaoToaThuTaiCung("Nô Bộc", khongkiep[i])) {
-            console.log(`${khongkiep[i]} toạ thủ cung Nô Bộc`);
-            keyArr.push(`${khongkiep[i]} toạ thủ cung Nô Bộc`);
-
-        }
-    }
+   
     for (let i = 0; i < dac_dia.length; i++) {
         for (let j = 0; j < khongkiep.length; j++) {
             if (isSaoToaThuTaiCungVaChi("Nô Bộc", dac_dia[i], khongkiep[j])) {
@@ -2729,8 +2712,7 @@ function LuanCachCucTuHoaNoBoc(keyArr) {
     const danthan = ["Dần", "Thân"];
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Hoá Lộc")) {
-        console.log("Hoá Lộc toạ thủ cung Nô Bộc");
-        keyArr.push("Hoá Lộc toạ thủ cung Nô Bộc");
+       
         if (kiemTraCachCuc("Hoá Lộc", ["Hóa Quyền", "Hóa Khoa", "Văn Xương", "Văn Khúc"])) {
             console.log("Hoá Lộc toạ thủ cung Nô Bộc gặp Hóa Quyền, Hóa Khoa, Văn Xương, Văn Khúc");
             keyArr.push("Hoá Lộc toạ thủ cung Nô Bộc gặp Hóa Quyền, Hóa Khoa, Văn Xương, Văn Khúc");
@@ -2754,8 +2736,7 @@ function LuanCachCucTuHoaNoBoc(keyArr) {
     }
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Hoá Quyền")) {
-        console.log("Hoá Quyền toạ thủ cung Nô Bộc");
-        keyArr.push("Hoá Quyền toạ thủ cung Nô Bộc");
+       
         if (kiemTraCachCuc("Hoá Quyền", ["Hóa Lộc", "Hóa Khoa", "Văn Xương", "Văn Khúc"])) {
             console.log("Hoá Quyền toạ thủ cung Nô Bộc gặp Hóa Lộc, Hóa Khoa, Văn Xương, Văn Khúc");
             keyArr.push("Hoá Quyền toạ thủ cung Nô Bộc gặp Hóa Lộc, Hóa Khoa, Văn Xương, Văn Khúc");
@@ -2791,8 +2772,7 @@ function LuanCachCucTuHoaNoBoc(keyArr) {
         }
     }
     if (isSaoToaThuTaiCung("Nô Bộc", "Hoá Khoa")) {
-        console.log("Hoá Khoa toạ thủ cung Nô Bộc");
-        keyArr.push("Hoá Khoa toạ thủ cung Nô Bộc");
+        
         if (kiemTraCachCuc("Hoá Khoa", ["Hóa Lộc", "Hóa Quyền", "Văn Xương", "Văn Khúc"])) {
             console.log("Hoá Khoa toạ thủ cung Nô Bộc gặp Hóa Lộc, Hóa Quyền, Văn Xương, Văn Khúc");
             keyArr.push("Hoá Khoa toạ thủ cung Nô Bộc gặp Hóa Lộc, Hóa Quyền, Văn Xương, Văn Khúc");
@@ -2820,8 +2800,7 @@ function LuanCachCucTuHoaNoBoc(keyArr) {
     }
 
     if (isSaoToaThuTaiCung("Nô Bộc", "Hóa Kỵ")) {
-        console.log("Hóa Kỵ toạ thủ cung Nô Bộc");
-        keyArr.push("Hóa Kỵ toạ thủ cung Nô Bộc");
+       
         if (kiemTraCachCuc("Hóa Kỵ", ["Hỏa Tinh", "Linh Tinh", "Kình Dương", "Đà La"])) {
             console.log("Hóa Kỵ toạ thủ cung Nô Bộc gặp Hỏa Tinh, Linh Tinh, Kình Dương, Đà La");
             keyArr.push("Hóa Kỵ toạ thủ cung Nô Bộc gặp Hỏa Tinh, Linh Tinh, Kình Dương, Đà La");
@@ -2990,14 +2969,6 @@ function LuanCachCucLucBaiTinhNoBoc(keyArr) {
     const songhao = ["Đại Hao", "Tiểu Hao"];
     const tangho = ["Tang Môn", "Bạch Hổ"];
 
-    for (let i = 0; i < songhao.length; i++) {
-        if (isSaoToaThuTaiCung("Nô Bộc", songhao[i])) {
-
-            console.log(songhao[i] + " toạ thủ tại cung Nô Bộc");
-            keyArr.push(songhao[i] + " toạ thủ tại cung Nô Bộc");
-
-        }
-    }
 
 
     for (let i = 0; i < songhao_dac.length; i++) {
@@ -3019,12 +2990,11 @@ function LuanCachCucLucBaiTinhNoBoc(keyArr) {
 
     for (let i = 0; i < tangho.length; i++) {
         if (isSaoToaThuTaiCung("Nô Bộc", tangho[i])) {
-            console.log("Cung Nô Bộc có " + tangho[i]);
-            keyArr.push("Cung Nô Bộc có " + tangho[i]);
+           
             for (let j = 0; j < tangho_dac.log; j++) {
                 if (isSaoToaThuTaiCungVaChi("Nô Bộc", tangho_dac[j], tangho[i])) {
-                    console.log("Cung Nô Bộc có " + tangho[i] + " toạ thủ cung Nô Bộc tại " + tangho_dac[j]);
-                    keyArr.push("Cung Nô Bộc có " + tangho[i] + " toạ thủ cung Nô Bộc tại " + tangho_dac[j]);
+                    console.log(tangho[i] + " toạ thủ cung Nô Bộc tại " + tangho_dac[j]);
+                    keyArr.push(tangho[i] + " toạ thủ cung Nô Bộc tại " + tangho_dac[j]);
                 }
             }
             if (lasoData.gioitinh === "Nữ") {
@@ -3090,8 +3060,7 @@ function LuanCachCucLucBaiTinhNoBoc(keyArr) {
 
     for (let i = 0; i < khochu.length; i++) {
         if (isSaoToaThuTaiCung("Nô Bộc", khochu[i])) {
-            console.log(khochu[i] + " toạ thủ cung Nô Bộc");
-            keyArr.push(khochu[i] + " toạ thủ cung Nô Bộc");
+           
             for (let j = 0; j < khochu_dac.length; j++) {
                 if (isSaoToaThuTaiCungVaChi("Nô Bộc", khochu_dac[j], khochu[i])) {
                     console.log((khochu[i] + " toạ thủ cung Nô Bộc tại " + khochu_dac[j]));
